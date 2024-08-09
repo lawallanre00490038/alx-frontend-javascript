df006770c0cfc3971a363c8381841d089f069836
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-08-09 08:36:43 +0100

    test

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,10 @@
 import ClassRoom from "./0-classroom.js";
-
-const first = new ClassRoom(19);
-const second = new ClassRoom(20);
-const third = new ClassRoom(34);
-
 const initializeRooms = () => {
+  const first = new ClassRoom(19);
+  const second = new ClassRoom(20);
+  const third = new ClassRoom(34);
   const arr = [first, second, third];
+
   return (arr);
 }
 export default initializeRooms;
